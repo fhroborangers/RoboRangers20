bfdce3fc60deeace6143945c2e433c0dbcdaf11a
Shadowtrap <arnav.mittal09@gmail.com>
2019-08-21 19:03:57 -0400

    Added organization folders and telebot helper class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Helper/TeleBot.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.Helper;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class TeleBot extends Robot {
+
+    public TeleBot(HardwareMap hwm, Telemetry tele){
+        super(hwm, tele);
+    }
+}
